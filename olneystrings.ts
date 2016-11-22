66c71fe4077e49b367e1f8a7a7164001d3b41e26
Tim Greaves <tdgreaves@hotmail.com>
2016-11-22 17:04:19 +0000

    commit2

---
+++ olneystrings.ts
@@ -0,0 +1,14 @@
+namespace olneystrings {
+
+    /**
+     * Gets the length of the specified string
+     */
+    //% blockId=olneystrings_lengthof
+    //% block="get length of  %text"
+    //% weight=10 
+    //% blockGap=8
+    export function LengthOf(text: string) : number {
+        return text.length;        
+    }   
+
+}
